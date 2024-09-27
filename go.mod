d0f5e04fdf2b1626a86bf0bd8b474c01c51e820d
Alain Liu <luumanhquan.91@gmail.com>
2024-09-27 23:31:04 +0700

    add code

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.22.2
 require (
 	github.com/bwmarrin/snowflake v0.3.0
 	github.com/gin-gonic/gin v1.10.0
+	github.com/go-redis/redis v6.15.9+incompatible
 	github.com/hashicorp/consul/api v1.29.4
 	github.com/prometheus/client_golang v1.20.4
 	github.com/sony/gobreaker v1.0.0
@@ -58,6 +59,8 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/onsi/ginkgo v1.16.5 // indirect
+	github.com/onsi/gomega v1.34.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
 	github.com/prometheus/client_model v0.6.1 // indirect
 	github.com/prometheus/common v0.55.0 // indirect
--- main.go
+++ main.go
@@ -4,11 +4,13 @@ import (
 	"context"
 	"encoding/base64"
 	"encoding/json"
+	"errors"
 	"io"
 	"log"
 	"math"
 	"net/http"
 	"os"
+	"reflect"
 	"strconv"
 	"strings"
 	"time"
@@ -16,6 +18,7 @@ import (
 	"github.com/bwmarrin/snowflake"
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/render"
+	"github.com/go-redis/redis"
 	consulapi "github.com/hashicorp/consul/api"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
@@ -333,4 +336,68 @@ func (c *ClientCircuitBreakerProxy) Send(endpoint string) (interface{}, error) {
 		return string(body), err
 	})
 	return data, err
+}
+
+type cacheHelper struct {
+	resdis *redis.Client
+}
+
+type CacheHelper interface {
+	GetInterface(ctx context.Context, key string, value interface{}) (interface{}, error)
+	Set(ctx context.Context, key string, value interface{}, exppiration time.Duration) error
+}
+
+func NewCacheHelper(dns string) CacheHelper {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     dns,
+		Password: "",
+		DB:       0,
+	})
+	return &cacheHelper{
+		resdis: rdb,
+	}
+}
+
+func (h *cacheHelper) GetInterface(ctx context.Context, key string, value interface{}) (interface{}, error) {
+	data, err := h.resdis.Get(key).Result()
+	if err != nil {
+		return nil, err
+	}
+
+	typeValue := reflect.TypeOf(data)
+	kind := typeValue.Kind()
+
+	var outData interface{}
+	switch kind {
+	case reflect.Ptr, reflect.Struct, reflect.Slice:
+		outData = reflect.New(typeValue).Interface()
+	default:
+		outData = reflect.Zero(typeValue).Interface()
+	}
+	err = json.Unmarshal([]byte(data), &outData)
+	if err != nil {
+		return nil, err
+	}
+	switch kind {
+	case reflect.Ptr, reflect.Struct, reflect.Slice:
+		return reflect.ValueOf(outData).Interface(), nil
+	}
+	var outValue interface{} = outData
+	if reflect.TypeOf(outData).ConvertibleTo(typeValue) {
+		outValueConverted := reflect.ValueOf(outData).Convert(typeValue)
+		outValue = outValueConverted.Interface()
+	}
+	if outValue == nil {
+		return nil, errors.New("")
+	}
+	return outData, nil
+}
+
+func (h *cacheHelper) Set(ctx context.Context, key string, value interface{}, exppiration time.Duration) error {
+	data, err := json.Marshal(&value)
+	if err != nil {
+		return err
+	}
+	_ = h.resdis.Set(key, data, exppiration)
+	return nil
 }
